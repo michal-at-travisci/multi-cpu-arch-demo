8c1a988f06893434c4b9ed17ba65591179ecd3d4
Michał Rybiński <54797573+michal-at-travisci@users.noreply.github.com>
2019-11-08 17:20:29 +0100

    Output env variable TRAVIS_CPU_ARCH to stdout

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,1 +1,18 @@
-#include <stdio>
+#include <iostream>
+#include <cstdlib>
+
+// Environment variable name as per https://docs.travis-ci.com/user/environment-variables#default-environment-variables
+const string env_name="TRAVIS_CPU_ARCH";
+
+int main()
+{
+  int ret_code = 0;
+  if (const char* env_p = std::getenv(env_name)) {
+    std::cout << "Your " + env_name + " is: " << env_p << "\n";
+  } else {
+    std::cout << "Didn't find env variable named: " + env_name;
+    ret_code = 1;
+  }
+  
+  exit(ret_code);
+}
