93b0c4d196260c99ca7c2e2523511f416af7c36c
Michał Rybiński <54797573+michal-at-travisci@users.noreply.github.com>
2019-11-08 20:07:59 +0100

    Update demo.cpp

--- src/demo.cpp
+++ src/demo.cpp
@@ -8,9 +8,9 @@ int main()
 {
   int ret_code = 0;
   if (const char* env_p = std::getenv(env_name)) {
-    std::cout << "Your " + env_name + " is: " << env_p << "\n";
+    std::cout << "Your " << env_name << " is: " << env_p << "\n";
   } else {
-    std::cout << "Didn't find env variable named: " + env_name;
+    std::cout << "Didn't find env variable named: " << env_name << "\n";
     ret_code = 1;
   }
   
