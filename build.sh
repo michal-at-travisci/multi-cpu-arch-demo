d1c42976f4078ad0edd1b80265f4589e85f76198
Michał Rybiński <54797573+michal-at-travisci@users.noreply.github.com>
2019-11-08 22:09:46 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+GREEN='\032[0;31m'
+NC='\033[0m'
+# display CPU info taken from /proc/cpuinfo
+echo -e " ${GREEN}CPU info:\n"
 cat /proc/cpuinfo | grep 'vendor' | uniq
 cat /proc/cpuinfo | grep 'model name' | uniq
+echo -e "${NC}\n"
+
 make info && make demo && \
-chmod +x ./build/demo && ./build/demo
+chmod +x ./build/demo 
+# run application we've just built
+echo "${GREEN}"
+./build/demo
+echo "${NC}"
