c6dac4407292428fff874a715a031c9861c74869
Michał Rybiński <54797573+michal-at-travisci@users.noreply.github.com>
2019-11-08 22:14:25 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# display CPU info taken from /proc/cpuinfo
-echo -e "\n CPU info from /proc/cpuinfo:\n"
-cat /proc/cpuinfo | grep 'vendor' | uniq
-cat /proc/cpuinfo | grep 'model name' | uniq
-echo -e "\n"
 # build app using Makefile and make binary executable
 make info && make demo && \
 chmod +x ./build/demo 
